30f4575d2a84ff5a1d22ee37ca91035d69e742f0
thisisaaronland <devnull@localhost>
2020-12-04 13:25:04 -0500

    update README

---
+++ README.go
@@ -0,0 +1,13 @@
+# go-whosonfirst-spatial-database-sqlite
+
+## Important
+
+This is work in progress and doesn't do anything yet.
+
+The goal is to have a package that conforms to the [database.SpatialDatabase](https://github.com/whosonfirst/go-whosonfirst-spatial#spatialdatabase) interface using [mattn/go-sqlite3](https://github.com/mattn/go-sqlite3) and SQLite's [RTree](https://www.sqlite.org/rtree.html) extension.
+
+## See also
+
+* https://github.com/whosonfirst/go-whosonfirst-spatial
+* https://github.com/mattn/go-sqlite3
+* https://www.sqlite.org/rtree.html
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/whosonfirst/go-whosonfirst-spatial-database-sqlite
+
+go 1.12								   
