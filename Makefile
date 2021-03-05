1ca28862da025640b2704f54899ede075b6d78e8
thisisaaronland <devnull@localhost>
2021-03-05 11:47:30 -0800

    filters

--- Makefile
+++ Makefile
@@ -12,6 +12,7 @@ make local:
 
 # test with:
 # curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{"latitude":37.616951,"longitude":-122.383747}'
+# curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{"latitude":37.616951,"longitude":-122.383747,"is_current":[1]}'
 
 local-query:
 	docker run -e PIP_MODE=lambda -e PIP_SPATIAL_DATABASE_URI=sqlite://?dsn=/usr/local/data/arch.db -p 9000:8080 point-in-polygon:latest /main
--- cmd/query/main.go
+++ cmd/query/main.go
@@ -4,15 +4,18 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/sfomuseum/go-flags/flagset"
 	_ "github.com/whosonfirst/go-whosonfirst-spatial-sqlite"
 	"github.com/whosonfirst/go-whosonfirst-spatial/database"
+	"github.com/whosonfirst/go-whosonfirst-spatial/filter"
 	"github.com/whosonfirst/go-whosonfirst-spatial/flags"
 	"github.com/whosonfirst/go-whosonfirst-spatial/geo"
 	"github.com/whosonfirst/go-whosonfirst-spatial/properties"
 	"github.com/whosonfirst/go-whosonfirst-spr"
-	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/sfomuseum/go-flags/flagset"
 	"log"
+	"net/url"
+	"strconv"
 )
 
 func main() {
@@ -30,7 +33,7 @@ func main() {
 	}
 
 	mode := fs.String("mode", "cli", "...")
-	
+
 	flags.Parse(fs)
 
 	err = flagset.SetFlagsFromEnvVars(fs, "PIP")
@@ -38,7 +41,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("Failed to assign flags from environment variables, %v", err)
 	}
-	
+
 	err = flags.ValidateCommonFlags(fs)
 
 	if err != nil {
@@ -55,33 +58,25 @@ func main() {
 	properties_uri, _ := flags.StringVar(fs, "properties-reader-uri")
 
 	// props, _ := flags.MultiStringVar(fs, "properties")
-	
+
 	ctx := context.Background()
 	db, err := database.NewSpatialDatabase(ctx, database_uri)
-	
+
 	if err != nil {
 		log.Fatalf("Failed to create database for '%s', %v", database_uri, err)
 	}
-	
+
 	// This is the meat of it which we're putting in its own function that
 	// can be invoked in both a CLI and a Lambda context
-	
-	query := func(ctx context.Context, latitude float64, longitude float64, props ...string) (interface{}, error) {
+
+	query := func(ctx context.Context, latitude float64, longitude float64, f filter.Filter, props ...string) (interface{}, error) {
 
 		c, err := geo.NewCoordinate(longitude, latitude)
-		
+
 		if err != nil {
 			return nil, fmt.Errorf("Failed to create new coordinate, %v", err)
 		}
 
-		// HOW TO FROM LAMBDA...
-		
-		f, err := flags.NewSPRFilterFromFlagSet(fs)
-		
-		if err != nil {
-			return nil, fmt.Errorf("Failed to create SPR filter, %v", err)
-		}
-		
 		var rsp interface{}
 
 		r, err := db.PointInPolygon(ctx, c, f)
@@ -91,17 +86,17 @@ func main() {
 		}
 
 		rsp = r
-		
+
 		if len(props) > 0 {
-			
+
 			pr, err := properties.NewPropertiesReader(ctx, properties_uri)
-			
+
 			if err != nil {
 				return nil, fmt.Errorf("Failed to create properties reader, %v", err)
 			}
-			
+
 			r, err := pr.PropertiesResponseResultsWithStandardPlacesResults(ctx, rsp.(spr.StandardPlacesResults), props)
-			
+
 			if err != nil {
 				return nil, fmt.Errorf("Failed to generate properties response, %v", err)
 			}
@@ -111,44 +106,106 @@ func main() {
 
 		return r, nil
 	}
-	
+
 	switch *mode {
 
 	case "cli":
-		
+
 		latitude, _ := flags.Float64Var(fs, "latitude")
 		longitude, _ := flags.Float64Var(fs, "longitude")
 
 		props, _ := flags.MultiStringVar(fs, "properties")
 
-		rsp, err := query(ctx, latitude, longitude, props...)
+		f, err := flags.NewSPRFilterFromFlagSet(fs)
+
+		if err != nil {
+			log.Fatalf("Failed to create SPR filter, %v", err)
+		}
+
+		rsp, err := query(ctx, latitude, longitude, f, props...)
 
 		if err != nil {
 			log.Fatalf("Failed to query, %v", err)
 		}
-		
+
 		enc, err := json.Marshal(rsp)
-		
+
 		if err != nil {
 			log.Fatalf("Failed to marshal results, %v", err)
 		}
-		
+
 		fmt.Println(string(enc))
-		
+
 	case "lambda":
 
+		// TODO: move this in to go-whosonfirst-spatial
+		// maybe an /aws/events or aws/lambda directory
+		// TBD: consider whether this is just aws specific
+		// or the ground work for a general purpose API
+		// (20210305/thisisaaronland)
+
 		type PointInPolygonEvent struct {
-			Latitude float64 `json:"latitude"`
-			Longitude float64 `json:"longitude"`
-			Properties []string `json:"properties"`
+			Latitude            float64  `json:"latitude"`
+			Longitude           float64  `json:"longitude"`
+			Properties          []string `json:"properties"`
+			Placetypes          []string `json:"placetypes,omitempty"`
+			Geometries          string   `json:"geometries,omitempty"`
+			AlternateGeometries []string `json:"alternate_geometries,omitempty"`
+			IsCurrent           []int    `json:"is_current,omitempty"`
+			IsCeased            []int    `json:"is_ceased,omitempty"`
+			IsDeprecated        []int    `json:"is_deprecated,omitempty"`
+			IsSuperseded        []int    `json:"is_superseded,omitempty"`
+			IsSuperseding       []int    `json:"is_superseding,omitempty"`
 		}
 
 		handler := func(ctx context.Context, ev PointInPolygonEvent) (interface{}, error) {
-			return query(ctx, ev.Latitude, ev.Longitude, ev.Properties...)
+
+			// TODO: move this in to go-whosonfirst-spatial
+			// as in a filter.NewSPRFilterFromPointInPolygonEvent
+			// method (20210305/thisisaaronland)
+
+			q := url.Values{}
+			q.Set("geometries", ev.Geometries)
+
+			for _, v := range ev.AlternateGeometries {
+				q.Add("alternate_geometry", v)
+			}
+
+			for _, v := range ev.Placetypes {
+				q.Add("placetype", v)
+			}
+
+			for _, v := range ev.IsCurrent {
+				q.Add("is_current", strconv.Itoa(v))
+			}
+
+			for _, v := range ev.IsCeased {
+				q.Add("is_ceased", strconv.Itoa(v))
+			}
+
+			for _, v := range ev.IsDeprecated {
+				q.Add("is_deprecated", strconv.Itoa(v))
+			}
+
+			for _, v := range ev.IsSuperseded {
+				q.Add("is_superseded", strconv.Itoa(v))
+			}
+
+			for _, v := range ev.IsSuperseding {
+				q.Add("is_superseding", strconv.Itoa(v))
+			}
+
+			f, err := filter.NewSPRFilterFromQuery(q)
+
+			if err != nil {
+				return nil, err
+			}
+
+			return query(ctx, ev.Latitude, ev.Longitude, f, ev.Properties...)
 		}
 
 		lambda.Start(handler)
-		
+
 	default:
 		log.Fatalf("Invalid or unsupported mode '%s'", *mode)
 	}
