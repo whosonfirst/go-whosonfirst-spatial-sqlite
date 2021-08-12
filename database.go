d703b89d08327f0da753763e59ec1d75d48da7db
thisisaaronland <aaron@localhost>
2021-08-11 18:12:08 -0700

    remove debugging

--- database.go
+++ database.go
@@ -26,7 +26,6 @@ import (
 	"github.com/whosonfirst/go-whosonfirst-uri"
 	"io"
 	"net/url"
-	golog "log"
 	"strings"
 	"sync"
 	"time"
@@ -121,7 +120,6 @@ func NewSQLiteSpatialDatabaseWithDatabase(ctx context.Context, uri string, sqlit
 	rtree_table, err := tables.NewRTreeTableWithDatabase(ctx, sqlite_db)
 
 	if err != nil {
-		golog.Println("POO", err)
 		return nil, err
 	}
 
